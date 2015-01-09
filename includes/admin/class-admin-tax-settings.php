a958b87d4f70a77eb386999cbffc645be0ca929d
Coen Jacobs <coenjacobs@gmail.com>
2015-01-09 08:49:02 +0100

    Only get categories in settings if API key is set

--- includes/admin/class-admin-tax-settings.php
+++ includes/admin/class-admin-tax-settings.php
@@ -6,11 +6,17 @@ class EDD_VIEU_Admin_Tax_Settings {
 	}
 
 	public function tax_settings( $settings ) {
-		$category_repo = new VIEU_Category_Repository();
+		$edd_options = get_option('edd_settings');
+
+		$categories = array();
 		$categories[0] = 'Select your category (optional)';
 
-		foreach ( $category_repo->get_categories() as $category ) {
-			$categories[ $category->id ] = $category->name;
+		if ( isset( $edd_options['vieu_api_key'] ) && ! empty( $edd_options['vieu_api_key'] ) ) {
+			$category_repo = new VIEU_Category_Repository();
+
+			foreach ( $category_repo->get_categories() as $category ) {
+				$categories[ $category->id ] = $category->name;
+			}
 		}
 
 		$settings_array = array(
