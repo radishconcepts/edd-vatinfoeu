fb071bea8ce40e5123a0574e2a68837d40c557b9
Coen Jacobs <coenjacobs@gmail.com>
2015-01-09 09:05:01 +0100

    Check for valid API key before query for rate

--- includes/core/class-tax.php
+++ includes/core/class-tax.php
@@ -9,20 +9,22 @@ class EDD_VIEU_Tax {
 		$edd_settings = get_option('edd_settings');
 
 		if ( isset( $edd_settings['vieu_enabled'] ) && '1' === $edd_settings['vieu_enabled'] ) {
-			$country_repo = new VIEU_Country_Repository();
-			$vieu_country = $country_repo->get_country_by_code( $country );
+			if ( isset( $edd_options['vieu_api_key'] ) && ! empty( $edd_options['vieu_api_key'] ) ) {
+				$country_repo = new VIEU_Country_Repository();
+				$vieu_country = $country_repo->get_country_by_code( $country );
 
-			// This method will return the default rate when country is not found in the API
-			// In most cases this means that a country is not within the EU.
-			if ( false === $vieu_country ) {
-				return $rate;
+				// This method will return the default rate when country is not found in the API
+				// In most cases this means that a country is not within the EU.
+				if ( false === $vieu_country ) {
+					return $rate;
+				}
+
+				$category_id = $edd_settings['vieu_category'];
+
+				$rate_calc = new VIEU_Rate();
+				$rate      = $rate_calc->get_rate( $vieu_country->id, $category_id );
+				$rate      = $rate->rate / 100;
 			}
-
-			$category_id = $edd_settings['vieu_category'];
-
-			$rate_calc = new VIEU_Rate();
-			$rate = $rate_calc->get_rate( $vieu_country->id, $category_id );
-			$rate = $rate->rate / 100;
 		}
 
 		return $rate;
