448d5a746649c3f968783cf62feabbb5864889d6
Coen Jacobs <coenjacobs@gmail.com>
2015-01-09 09:06:24 +0100

    Conditionally hook function instead of condition logic

--- includes/core/class-tax.php
+++ includes/core/class-tax.php
@@ -2,31 +2,30 @@
 
 class EDD_VIEU_Tax {
 	public function __construct() {
-		add_filter( 'edd_tax_rate', array( $this, 'matched_tax_rates' ), 10, 2 );
+		if ( isset( $edd_settings['vieu_enabled'] ) && '1' === $edd_settings['vieu_enabled'] ) {
+			if ( isset( $edd_options['vieu_api_key'] ) && ! empty( $edd_options['vieu_api_key'] ) ) {
+				add_filter( 'edd_tax_rate', array( $this, 'matched_tax_rates' ), 10, 2 );
+			}
+		}
 	}
 
 	public function matched_tax_rates( $rate, $country ) {
 		$edd_settings = get_option('edd_settings');
 
-		if ( isset( $edd_settings['vieu_enabled'] ) && '1' === $edd_settings['vieu_enabled'] ) {
-			if ( isset( $edd_options['vieu_api_key'] ) && ! empty( $edd_options['vieu_api_key'] ) ) {
-				$country_repo = new VIEU_Country_Repository();
-				$vieu_country = $country_repo->get_country_by_code( $country );
+		$country_repo = new VIEU_Country_Repository();
+		$vieu_country = $country_repo->get_country_by_code( $country );
 
-				// This method will return the default rate when country is not found in the API
-				// In most cases this means that a country is not within the EU.
-				if ( false === $vieu_country ) {
-					return $rate;
-				}
-
-				$category_id = $edd_settings['vieu_category'];
-
-				$rate_calc = new VIEU_Rate();
-				$rate      = $rate_calc->get_rate( $vieu_country->id, $category_id );
-				$rate      = $rate->rate / 100;
-			}
+		// This method will return the default rate when country is not found in the API
+		// In most cases this means that a country is not within the EU.
+		if ( false === $vieu_country ) {
+			return $rate;
 		}
 
+		$category_id = $edd_settings['vieu_category'];
+
+		$rate_calc = new VIEU_Rate();
+		$rate      = $rate_calc->get_rate( $vieu_country->id, $category_id );
+		$rate      = $rate->rate / 100;
 		return $rate;
 	}
 }
