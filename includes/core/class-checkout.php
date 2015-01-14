9e0e18c04f4300f4ed8b2b110535b3a09bbf9074
Coen Jacobs <coenjacobs@gmail.com>
2015-01-14 09:15:16 +0100

    Validator for VAT number field

--- includes/core/class-checkout.php
+++ includes/core/class-checkout.php
@@ -1,8 +1,14 @@
 <?php
 
 class EDD_VIEU_Checkout {
+	private $countries = array();
+
 	public function __construct() {
-		add_action('edd_cc_billing_bottom', array( $this, 'add_vat_number_field' ) );
+		$country_repo = new VIEU_Country_Repository();
+		$this->countries = $country_repo->get_countries();
+
+		add_action( 'edd_cc_billing_bottom', array( $this, 'add_vat_number_field' ) );
+		add_filter( 'edd_checkout_error_checks', array( $this, 'check_vat_number' ), 10, 2 );
 	}
 
 	public function add_vat_number_field() {
@@ -15,5 +21,18 @@ class EDD_VIEU_Checkout {
 				<input type="text" size="6" name="vieu_vat_number" id="vieu_vat_number" class="vieu_vat_number edd-input" placeholder="VAT Number"/>
 			</p>
 	<?php
+	}
+
+	public function check_vat_number( $valid_data, $data ) {
+		if ( isset( $data['vieu_vat_number'] ) && ! empty( $data['vieu_vat_number'] ) ) {
+			if ( ! $this->validate( $data['vieu_vat_number'], 'NL' ) ) {
+				edd_set_error( 'euvi-invalid-vat', sprintf( 'The VAT number (%s) is invalid for your billing country.', $data['vieu_vat_number'] ) );
+			}
+		}
+	}
+
+	private function validate( $vat_number, $country_code ) {
+		$validator = new VIEU_VAT_Validator();
+		return $validator->validate_vat($country_code, $vat_number);
 	}
 }
